0d729ff0f6f3903dbed74d694a2ef9fe342e6707
Joe <joseph.rall@skadden.com>
2016-06-20 16:28:18 -0400

    Replacing Mergely for Monaco.

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,37 @@
-import Editor from "editor";
+class Editor {
+    init() {
+        console.log("Editor Loaded");
+        (<any>require).config({ paths: { 'vs': './node_modules/monaco-editor/min/vs' } });
+        (<any>require)(['vs/editor/editor.main'], function () {
+            var diffEditor = monaco.editor.createDiffEditor(document.getElementById('container'));
+
+            //diffEditor.updateOptions({ 'theme': "vs-dark" });
+            function updateLayout() {
+                $("#container").height($(window).height() - $("#header").height() - 80);
+                diffEditor.layout();
+            }
+
+            $(window).resize(function () {
+                updateLayout();
+            });
+
+            $("#swap_button").on("click", function () {
+                var model = diffEditor.getModel();
+                var tmp = model.original;
+                model.original = model.modified;
+                model.modified = tmp;
+                diffEditor.setModel(model);
+            });
+
+            diffEditor.setModel({
+                original: monaco.editor.createModel("", 'text'),
+                modified: monaco.editor.createModel("", 'text'),
+            });
+
+            updateLayout();
+        });
+    }
+}
 
 var x = new Editor();
 x.init();
-
--- main.js
+++ main.js
@@ -11,8 +11,7 @@ let mainWindow
 
 function createWindow() {
   // Create the browser window.
-  //mainWindow = new BrowserWindow({width: 800, height: 600})
-  mainWindow = new BrowserWindow();
+  mainWindow = new BrowserWindow({width: 1400, height: 600})
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
